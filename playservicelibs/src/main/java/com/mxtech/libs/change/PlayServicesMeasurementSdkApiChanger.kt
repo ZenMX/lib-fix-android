7a142d9b3045ac4a40f9bd2231e02292835337a4
LiaoPengwei <pengwei.liao@mxplayer.in>
2021-08-05 17:34:17 +0800

    [LPW] change play services measurement sdk api lib

---
+++ playservicelibs/src/main/java/com/mxtech/libs/change/PlayServicesMeasurementSdkApiChanger.kt
@@ -0,0 +1,36 @@
+package com.mxtech.libs.change
+
+import javassist.ClassPool
+import javassist.CtNewMethod
+import java.io.File
+
+/**
+ * Created by pengwei.liao on 2021/8/4.
+ */
+class PlayServicesMeasurementSdkApiChanger : BaseChanger() {
+
+    override fun libName(): String {
+        return "play-services-measurement-sdk-api-18.0.2.aar"
+    }
+
+    override fun changeLib(buildRoot: File) {
+        val pool = ClassPool.getDefault()
+        val source = File(buildRoot, "aar/classes.jar")
+        pool.appendClassPath(source.absolutePath)
+
+        val cc = pool["com.google.android.gms.internal.measurement.zzbr"]
+        val zzW = cc.getDeclaredMethod("zzW")
+        val zzWNew = CtNewMethod.copy(zzW, "zzWNew", cc, null)
+        cc.addMethod(zzWNew)
+        zzW.setBody(
+            "{" +
+                    "android.util.Log.e(\"HOOK_LOG\", \"impl class:\" + $0.zzk.getClass().getName());" +
+                    "$0.zzWNew($$);" +
+                    "}"
+
+        )
+
+        cc.writeFile(File(buildRoot, "jar").absolutePath)
+    }
+
+}
