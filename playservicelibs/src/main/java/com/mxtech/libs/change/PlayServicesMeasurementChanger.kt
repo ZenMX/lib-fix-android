c02bc58f292369f72030e9e318fb226d07d4ca8d
LiaoPengwei <pengwei.liao@mxplayer.in>
2021-08-06 13:40:59 +0800

    [LPW] change play services measurement lib, update version to 7

--- playservicelibs/src/main/java/com/mxtech/libs/change/PlayServicesMeasurementChanger.kt
+++ playservicelibs/src/main/java/com/mxtech/libs/change/PlayServicesMeasurementChanger.kt
@@ -1,6 +1,7 @@
 package com.mxtech.libs.change
 
 import javassist.ClassPool
+import javassist.CtClass
 import javassist.CtNewMethod
 import java.io.File
 
@@ -18,33 +19,53 @@ class PlayServicesMeasurementChanger : BaseChanger() {
         val source = File(buildRoot, "aar/classes.jar")
         pool.appendClassPath(source.absolutePath)
 
-        var cc = pool["com.google.android.gms.measurement.internal.zzen"]
-        val constructor = cc.constructors[0]
-        constructor.insertAfter(
-            "java.util.Iterator it = zzf.entrySet().iterator();" +
-                    "java.lang.StringBuilder sb = new java.lang.StringBuilder();" +
-                    "while (it.hasNext()) {" +
-                    "    java.util.Map.Entry entry = it.next();" +
-                    "    sb.append(entry.getKey()).append(\"=\").append(entry.getValue()).append(\";\");" +
-                    "}" +
-                    "android.util.Log.println(2, \"FA-SVC\", \"map=\" + sb.toString());"
-        )
-        var methods = cc.methods
-        for (method in methods) {
-            if (method.methodInfo.codeAttribute == null) continue
-            try {
-                method.insertBefore(
-                    "android.util.Log.println(2, \"FA-SVC\", \"=============${method.longName}\");" +
-                            "android.util.Log.v(\"FA-SVC\", \"=============${method.longName}\");"
-                )
-            } catch (e: Exception){
-                println("method:${method.longName} insert log exception")
+        val classNames = collectClassName(File(buildRoot, "jar"))
+        var cc: CtClass
+        for(clsName in classNames) {
+            cc = pool[clsName]
+            if(cc.isAnnotation || cc.isEnum || cc.isInterface || cc.isInterface || cc.isArray || cc.isPrimitive) continue
+            val methods = cc.methods
+            for (method in methods) {
+                try {
+                    if (method.methodInfo.codeAttribute == null) continue
+                    method.insertBefore(
+                        "android.util.Log.println(2, \"FA-SVC\", \"=============${method.longName}\");" +
+                                "android.util.Log.v(\"FA-SVC\", \"=============${method.longName}\");"
+                    )
+                } catch (e: Exception){
+                    println("method:${method.longName} insert log exception")
+                }
             }
+            cc.writeFile(File(buildRoot, "jar").absolutePath)
         }
 
-        cc.writeFile(File(buildRoot, "jar").absolutePath)
-
-        cc = pool["com.google.android.gms.measurement.internal.zzkd"]
+//        var cc = pool["com.google.android.gms.measurement.internal.zzen"]
+//        val constructor = cc.constructors[0]
+//        constructor.insertAfter(
+//            "java.util.Iterator it = zzf.entrySet().iterator();" +
+//                    "java.lang.StringBuilder sb = new java.lang.StringBuilder();" +
+//                    "while (it.hasNext()) {" +
+//                    "    java.util.Map.Entry entry = it.next();" +
+//                    "    sb.append(entry.getKey()).append(\"=\").append(entry.getValue()).append(\";\");" +
+//                    "}" +
+//                    "android.util.Log.println(2, \"FA-SVC\", \"map=\" + sb.toString());"
+//        )
+//        var methods = cc.methods
+//        for (method in methods) {
+//            if (method.methodInfo.codeAttribute == null) continue
+//            try {
+//                method.insertBefore(
+//                    "android.util.Log.println(2, \"FA-SVC\", \"=============${method.longName}\");" +
+//                            "android.util.Log.v(\"FA-SVC\", \"=============${method.longName}\");"
+//                )
+//            } catch (e: Exception){
+//                println("method:${method.longName} insert log exception")
+//            }
+//        }
+//
+//        cc.writeFile(File(buildRoot, "jar").absolutePath)
+//
+//        cc = pool["com.google.android.gms.measurement.internal.zzkd"]
 //        val zzF = cc.getDeclaredMethod("zzF")
 //        val zzFNew = CtNewMethod.copy(zzF, "zzFNew", cc, null)
 //        cc.addMethod(zzFNew)
@@ -55,34 +76,34 @@ class PlayServicesMeasurementChanger : BaseChanger() {
 //                    "$0.zzFNew();" +
 //                    "}"
 //        )
-        methods = cc.methods
-        for (method in methods) {
-            if (method.methodInfo.codeAttribute == null) continue
-            try {
-                method.insertBefore(
-                    "android.util.Log.println(2, \"FA-SVC\", \"=============${method.longName}\");" +
-                            "android.util.Log.v(\"FA-SVC\", \"=============${method.longName}\");"
-                )
-            } catch (e: Exception){
-                println("method:${method.longName} insert log exception")
-            }
-        }
-        cc.writeFile(File(buildRoot, "jar").absolutePath)
-
-        cc = pool["com.google.android.gms.measurement.internal.zzkf"]
-        methods = cc.methods
-        for (method in methods) {
-            if (method.methodInfo.codeAttribute == null) continue
-            try {
-                method.insertBefore(
-                    "android.util.Log.println(2, \"FA-SVC\", \"=============${method.longName}\");" +
-                            "android.util.Log.v(\"FA-SVC\", \"=============${method.longName}\");"
-                )
-            } catch (e: Exception){
-                println("method:${method.longName} insert log exception")
-            }
-        }
-        cc.writeFile(File(buildRoot, "jar").absolutePath)
+//        methods = cc.methods
+//        for (method in methods) {
+//            if (method.methodInfo.codeAttribute == null) continue
+//            try {
+//                method.insertBefore(
+//                    "android.util.Log.println(2, \"FA-SVC\", \"=============${method.longName}\");" +
+//                            "android.util.Log.v(\"FA-SVC\", \"=============${method.longName}\");"
+//                )
+//            } catch (e: Exception){
+//                println("method:${method.longName} insert log exception")
+//            }
+//        }
+//        cc.writeFile(File(buildRoot, "jar").absolutePath)
+//
+//        cc = pool["com.google.android.gms.measurement.internal.zzkf"]
+//        methods = cc.methods
+//        for (method in methods) {
+//            if (method.methodInfo.codeAttribute == null) continue
+//            try {
+//                method.insertBefore(
+//                    "android.util.Log.println(2, \"FA-SVC\", \"=============${method.longName}\");" +
+//                            "android.util.Log.v(\"FA-SVC\", \"=============${method.longName}\");"
+//                )
+//            } catch (e: Exception){
+//                println("method:${method.longName} insert log exception")
+//            }
+//        }
+//        cc.writeFile(File(buildRoot, "jar").absolutePath)
 
 //        val zzh = cc.getDeclaredMethod("zzh")
 //        val zzhNew = CtNewMethod.copy(zzh, "zzhNew", cc, null)
@@ -98,7 +119,24 @@ class PlayServicesMeasurementChanger : BaseChanger() {
 //
 //        )
 
-        cc.writeFile(File(buildRoot, "jar").absolutePath)
+//        cc.writeFile(File(buildRoot, "jar").absolutePath)
+    }
+
+    fun collectClassName(rootDir: File) : List<String>{
+        val list = ArrayList<String>()
+        val files = rootDir.listFiles()
+        if(files != null && files.isNotEmpty()) {
+            for(file in files) {
+                if(file.isDirectory) {
+                    list.addAll(collectClassName(file))
+                } else {
+                    if(!file.path.endsWith(".class")) continue
+                    val name = file.path.replace("/", ".").replace(".class", "").replace("..playservicelibs.build.play.jar.", "")
+                    list.add(name)
+                }
+            }
+        }
+        return list
     }
 
 }
